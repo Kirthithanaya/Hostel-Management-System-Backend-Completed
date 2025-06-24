de43dd48d998ecee915133f8d065a6dd1539eaf5
Kirthithanaya <kkeerthika546@gmail.com>
2025-06-24 18:24:01 +0800

    Added UserRole

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,9 +9,10 @@ import { allowRoles, protect } from "../middlewares/authMiddleware.js";
 const router = express.Router();
 
 // Admin-only routes
-router.get("/getall", protect, allowRoles("admin"), getAllUsers);
-router.put("/:id/role", protect, allowRoles("admin"), updateUserRole);
+router.get("/getall", protect, allowRoles("admin,resident"), getAllUsers);
+router.put("/:id/role", protect, allowRoles("admin, resident"), updateUserRole);
 
-router.delete("/:id", protect, allowRoles("admin"), deleteUser);
+router.delete("/:id", protect, allowRoles("admin, resident"), deleteUser);
+
 
 export default router;
